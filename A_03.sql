2cb2b05e3ef297bffee7ef9d4a322c079aec849e
Lai Chun Kong <i_am_gary_lai@yahoo.com>
2025-04-12 15:55:30 +0800

    Question 03

---
+++ A_03.sql
@@ -0,0 +1,13 @@
+/*
+1) Find the name, population, and area of the big countries.
+*/
+
+SELECT
+    name,
+    population,
+    area
+FROM
+    world
+WHERE
+    area >= 3000000 OR
+    population >= 25000000;
