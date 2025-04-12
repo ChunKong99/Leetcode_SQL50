21da21fe3ea502d756104581141fa802320e01cb
Lai Chun Kong <i_am_gary_lai@yahoo.com>
2025-04-12 16:04:18 +0800

    update

--- A_04.sql
+++ A_04.sql
@@ -1,8 +1,9 @@
 # Write your MySQL query statement below
 /*
 1) Find all the authors that viewed at least one of their own articles
-	a) find authors
+	a) find viewer_id
 	b) filter by author_id = viewer_id
+    c) use DISTINCT to avoid duplicates
 2) Return the result table sorted by id in ascending order.
 	a) ORDER BY id ASC
 */
